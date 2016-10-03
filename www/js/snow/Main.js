95ce3fd987af968b9bd2b28668d04953eafcb4d0
EthanLiu <feng_yun_3212@163.com>
2016-10-03 23:41:09 +0800

    开始动手写ionic页面，知易行难

--- www/js/snow/Main.js
+++ www/js/snow/Main.js
@@ -1,42 +1,60 @@
-/**
- * Created by SevenLiu on 16/10/2.
- */
-angular.module('snow_berg',['ionic'])
-  .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+angular.module('snow_berg', ['ionic'])
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
-  })
-  .config(function($stateProvider,$urlRouteProvider)
-{
-  $stateProvider
-    .state('tabs',{
-    url:"/tab",
-    abstract:true,
-    templateUrl:"view/tabs.html"
-  })
-    .state('tabs.home',{
-      url: "/home",
-      views: {
-        'home-tab': {
-          templateUrl: "view/home.html",
-          controller: 'HomeTabCtrl'
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('tabs', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+      .state('tabs.home', {
+        url: "/home",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/home.html",
+            controller: 'HomeTabCtrl'
+          }
         }
-      }
-  });
-  $urlRouterProvider.otherwise("/tab/home");
-})
+      })
+      .state('tabs.facts', {
+        url: "/facts",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts.html"
+          }
+        }
+      })
+      .state('tabs.facts2', {
+        url: "/facts2",
+        views: {
+          'home-tab': {
+            templateUrl: "templates/facts2.html"
+          }
+        }
+      })
+      .state('tabs.about', {
+        url: "/about",
+        views: {
+          'about-tab': {
+            templateUrl: "templates/about.html"
+          }
+        }
+      })
+      .state('tabs.contact', {
+        url: "/contact",
+        views: {
+          'contact-tab': {
+            templateUrl: "templates/contact.html"
+          }
+        }
+      });
 
-.controller('HomeTabCtrl', function($scope) {
-  console.log('HomeTabCtrl');
-});
+
+    $urlRouterProvider.otherwise("/tab/home");
+
+  })
+
+  .controller('HomeTabCtrl', function($scope) {
+    console.log('HomeTabCtrl');
+  });
