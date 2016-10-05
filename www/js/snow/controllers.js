520d0282c2c534ed4fe7ea76ff8a2c3e7cc7c983
EthanLiu <feng_yun_3212@163.com>
2016-10-06 00:14:59 +0800

    尝试模块化

--- www/js/snow/controllers.js
+++ www/js/snow/controllers.js
@@ -1,6 +1,24 @@
 /**
  * Created by SevenLiu on 16/10/2.
  */
-angular.module('snow_berg.controllers', [])
+var app = angular.module('homePage', [ionic]);
 
-  .controller('HomeTabCtrl', function($scope) {});
+  app.controller('newsCtrl', function($scope) {
+    $scope.flightNews = [
+      {
+        "src":"img/avatar.jpg",
+        "title":"国庆飞行任务重",
+        "desc":"超负荷运转下如何保证安全飞行"
+      },
+      {
+        "src":"img/avatar.jpg",
+        "title":"国庆飞行任务重",
+        "desc":"超负荷运转下如何保证安全飞行"
+      },
+      {
+        "src":"img/avatar.jpg",
+        "title":"国庆飞行任务重",
+        "desc":"超负荷运转下如何保证安全飞行"
+      }
+    ];
+  });
